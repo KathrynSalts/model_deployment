4a416cd6042edd9df8ae74e34aaa0b2814cd858d
Kathryn Salts <kathrynsalts@gmail.com>
2019-05-21 14:54:13 -0500

    added first form with greeting

--- server.py
+++ server.py
@@ -11,3 +11,23 @@ def the_home_page():
 def dice_page():
     number = random.randint(1,6)
     return render_template('dice.html', dice_roll = number)
+
+@app.route('/my-first-form')
+def my_first_form():
+    return render_template('first_form.html')
+
+from flask import request
+
+@app.route('/make-greeting', methods=['POST'])
+def handle_form_submission():
+    name = request.form['name']
+    title = request.form['title']
+
+    greeting = 'Hello, '
+
+    if title != '':
+        greeting += title + ' '
+
+    greeting += name + '!'
+
+    return render_template('greeting.html', greeting=greeting)
